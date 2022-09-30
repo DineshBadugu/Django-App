e0337f5599bd797543bb438bd8404bdf03f41033
DineshBadugu <badugudinesh94@gmail.com>
2022-09-30 09:52:09 +0530

    change some change son url.py

--- Dineshtech/Dineshtech/urls.py
+++ Dineshtech/Dineshtech/urls.py
@@ -1,4 +1,4 @@
-"""Dineshtech URL Configuration
+"""Dineshtech URL Configuratio
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
